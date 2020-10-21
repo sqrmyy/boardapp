5ddf9d49709c456acc772cb90602b6e5ad151a85
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-10-21 09:56:54 +0900

    Create Post mutation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { API, graphqlOperation } from "aws-amplify";
+import { listPosts } from './graphql/queries';
+import { createPost } from './graphql/mutations';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    posts: [],
+    title: "",
+    content: ""
+  }
+
+  async componentDidMount() {
+    try {
+      const posts = await API.graphql(graphqlOperation(listPosts))
+      console.log('posts: ', posts)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  createPost = async () => {
+    // バリデーションチェック
+    if (this.state.title === '' || this.state.content === '') return
+
+    // 新規登録 mutation
+    const createPostInput = {
+      title: this.state.title,
+      content: this.state.content
+    }
+
+    // 登録処理
+    try {
+      const posts = [...this.state.posts, createPostInput]
+      this.setState({ posts: posts, title: "", content: "" })
+      await API.graphql(graphqlOperation(createPost, { input: createPostInput }))
+      console.log('createPostInput: ', createPostInput)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          タイトル
+        <input value={this.state.title} name="title" onChange={this.onChange}></input>
+        </div>
+        <div>
+          内容
+        <input value={this.state.content} name="content" onChange={this.onChange}></input>
+        </div>
+        <button onClick={this.createPost}>追加</button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,36 +1,30 @@
-/* eslint-disable */
+// eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const getPost = /* GraphQL */ `
-  query GetPost($id: ID!) {
-    getPost(id: $id) {
+export const getPost = `query GetPost($id: ID!) {
+  getPost(id: $id) {
+    id
+    title
+    content
+    price
+    rating
+  }
+}
+`;
+export const listPosts = `query ListPosts(
+  $filter: ModelPostFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
       id
       title
       content
       price
       rating
-      createdAt
-      updatedAt
     }
+    nextToken
   }
-`;
-export const listPosts = /* GraphQL */ `
-  query ListPosts(
-    $filter: ModelPostFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        title
-        content
-        price
-        rating
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
+}
 `;
